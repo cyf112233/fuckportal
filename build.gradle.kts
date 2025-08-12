c15df49d008685d0ebe4fa77d0438ea8c06e0bd4
cyf112233 <frank13967973981@163.com>
2025-08-12 15:20:26 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,14 @@ plugins {
     id ("com.gradleup.shadow") version "8.3.0"
 }
 
-group = "byd.cxkcxkckx.fuckportal"
-version = "1.0.0"
+val buildversion: String = if (project.hasProperty("version")) {
+    project.property("version") as String
+} else {
+    "unspecified"
+}
+
+group = "byd.cxkcxkckx"
+version = buildversion
 val targetJavaVersion = 8
 val shadowGroup = "top.mrxiaom.example.libs"
 
